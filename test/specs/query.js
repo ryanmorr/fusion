7d1bf3577f636110b28601efefdff4834d9e7934
Ryan Morr <rm.morr@gmail.com>
2022-02-28 02:23:19 -0500

    Improve tests for query store

--- test/specs/query.js
+++ test/specs/query.js
@@ -11,6 +11,58 @@ describe('query', () => {
         expect(elements.get()).to.deep.equal([element1, element2]);
     });
 
+    it('should call subscribes when matching elements are added to the DOM', (done) => {
+        const element1 = createElement('div', {className: 'foo'});
+
+        const elements = query('.foo');
+
+        const spy = sinon.spy();
+        elements.subscribe(spy);
+
+        expect(spy.callCount).to.equal(1);
+        expect(spy.args[0]).to.deep.equal([[element1]]);
+
+        const element2 = createElement('div', {className: 'foo'});
+
+        wait(() => {
+            expect(spy.callCount).to.equal(2);
+            expect(spy.args[1][0]).to.deep.equal([element1, element2]);
+            expect(spy.args[1][1]).to.deep.equal([element1]);
+            expect(spy.args[1][2]).to.deep.equal([element2]);
+            expect(spy.args[1][3]).to.deep.equal([]);
+            expect(elements.get()).to.deep.equal([element1, element2]);
+
+            done();
+        });
+    });
+
+    it('should call subscribes when matching elements are removed from the DOM', (done) => {
+        const element1 = createElement('div', {className: 'foo'});
+        const element2 = createElement('div', {className: 'foo'});
+        const element3 = createElement('div', {className: 'foo'});
+
+        const elements = query('.foo');
+
+        const spy = sinon.spy();
+        elements.subscribe(spy);
+
+        expect(spy.callCount).to.equal(1);
+        expect(spy.args[0]).to.deep.equal([[element1, element2, element3]]);
+
+        element1.remove();
+
+        wait(() => {
+            expect(spy.callCount).to.equal(2);
+            expect(spy.args[1][0]).to.deep.equal([element2, element3]);
+            expect(spy.args[1][1]).to.deep.equal([element1, element2, element3]);
+            expect(spy.args[1][2]).to.deep.equal([]);
+            expect(spy.args[1][3]).to.deep.equal([element1]);
+            expect(elements.get()).to.deep.equal([element2, element3]);
+
+            done();
+        });
+    });
+
     it('should call subscribes when matching elements are added and removed from the DOM', (done) => {
         const element1 = createElement('div', {className: 'foo'});
 
@@ -48,6 +100,28 @@ describe('query', () => {
     
                 done();
             });
+        });
+    });
+
+    it('should not call subscribes if the DOM has changed but the elements are the same', (done) => {
+        const element1 = createElement('div', {className: 'foo'});
+        const element2 = createElement('div', {className: 'foo'});
+    
+        const elements = query('.foo');
+    
+        const spy = sinon.spy();
+        elements.subscribe(spy);
+    
+        expect(spy.callCount).to.equal(1);
+        expect(spy.args[0]).to.deep.equal([[element1, element2]]);
+    
+        element2.remove();
+        document.body.appendChild(element2);
+    
+        wait(() => {
+            expect(spy.callCount).to.equal(1);
+            
+            done();
         });
     });
 
