57418865259a72c9d3763f4aba18ed57b2150249
Ryan Morr <rm.morr@gmail.com>
2023-04-07 14:01:18 -0400

    Remove addListener support

--- src/media.js
+++ src/media.js
@@ -3,11 +3,7 @@ import defineStore from '@ryanmorr/define-store';
 export const media = defineStore((get, set) => (query) => {
     const mq = matchMedia(query);
     const setValue = () => set(mq.matches);
-    if ('addEventListener' in mq) {
-        mq.addEventListener('change', setValue);
-    } else {
-        mq.addListener(setValue);
-    }
+    mq.addEventListener('change', setValue);
     setValue();
     return {
         css: '@media ' + query,
